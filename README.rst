64cb1292e3d688fb0defa465cbc595ce30cbb77b
Artturi Hämäläinen <artturi_hamalainen@hotmail.com>
2020-02-06 20:06:25 +0200

    project init

---
+++ README.rst
@@ -0,0 +1,5 @@
+Program will get info about certain German words
+
+For starting program you need to have list of german words separated by lines with or without comas
+
+python app.py pathToTextFIle verp (or adj) 
---
+++ cleanWordInput.py
@@ -0,0 +1,7 @@
+
+def clean_word_from_spaces_commas(word):
+    word_spaces_removed = word.strip()
+    return word_spaces_removed[:-1] if word_spaces_removed[-1:] == ',' else word_spaces_removed
+
+
+print(clean_word_from_spaces_commas('asd,   '))
